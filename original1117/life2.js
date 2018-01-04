5d50df0a8ab7e5b04c826b06dceab178052d5323
Seth Brady <sethgabrielbrady@gmail.com>
2018-01-03 23:36:10 -0500

    init commit

---
+++ original1117/life2.js
@@ -0,0 +1,129 @@
+const MoveDist = 10;
+const BaseSpeed = 600;
+const BuggPopulation = 10;
+
+let buggC = document.getElementsByClassName("bugg");
+let yPos = document.getElementById("yPos");
+let xPos = document.getElementById("xPos");
+let windowDim = document.getElementById("windowDim");
+let buggField = document.getElementById("buggField");
+let x = window.innerWidth;
+let y = window.innerHeight;
+let buggCount = 0;
+let startCount = 0;
+let buggyCount = 0;
+let newBugg = 1;
+windowDim.innerHTML = "Width:  " + x +"  Height:  " + y;
+
+//Bugg Constuctor
+function Bugg (name){
+  buggyCount = buggyCount + 1;
+  let rngSpeedModifier;
+  let rngX;
+  let rngy;
+
+  rngX = rngControl(x);
+  rngY = rngControl(y);
+  buggR = rngControl(255);
+  buggG = rngControl(255);
+  buggB = rngControl(255);
+  rngSpeedModifier = rngControl(10);
+  buggCount = buggCount + 1;
+
+  this.name = name;
+  this.number = buggCount;
+  this.buggSpeed = BaseSpeed * (rngSpeedModifier/10);
+  this.health = rngControl(10);
+  this.startXY = [rngX, rngY];
+  this.buggColor ="rgb("+buggR+","+ buggG+","+ buggB+");";
+
+  
+  this.stats = name +'s stats are: Speed: ['+
+              this.buggSpeed +'], ID:['+
+              this.number+'], XY POS: ['+
+              this.startXY + '],  health: ['+
+              this.health + '], color: ['+
+              this.buggColor +
+              ']';
+  console.log(this.stats);
+  document.getElementById("count").innerHTML = buggCount;
+}
+
+//RNG Controller
+function rngControl(number){
+  let newNum = Math.floor((Math.random() * number) + 1);
+  return newNum;
+}
+
+//Controls the random movement
+function buggMovement(buggID){
+  let buggMade = buggID;
+  buggEL = buggID;
+  buggMade = new Bugg(buggID);
+  let buggNew = document.getElementById(buggEL);
+  let moveSpeed = buggMade.buggSpeed;
+  let bColor = buggMade.buggColor;
+  //set the random position inside start
+  let buggYPos = rngControl(x);
+  let buggXPos = rngControl(y);
+  console.log(buggXPos + ':' + buggYPos);
+
+  setInterval(function(){
+    //keep this inside the setInterval function.
+    let rngMove = rngControl(4);
+    //stops everything if a bugs goes out of bounds
+    if (buggYPos >= x || buggYPos <= 0){
+      return 0;
+    }
+    if (buggXPos >= y || buggXPos <= 0){
+      return 0;
+    }
+
+    if (rngMove === 1){
+      if (buggYPos <= 50){
+        buggYPos = buggYPos + MoveDist;
+      }else{
+        buggYPos = buggYPos - MoveDist;
+      }
+      buggNew.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-left:4px solid white; background-color:"+bColor+'"');
+    }else if (rngMove === 2){
+      if (buggYPos >= x - 50){
+        buggYPos = buggYPos - MoveDist;
+      }else{
+        buggYPos = buggYPos + MoveDist;
+      }
+      buggNew.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-right:4px solid white;  background-color:"+bColor+'"');
+    }else if (rngMove === 3){
+      if (buggXPos >= y - 50){
+        buggXPos = buggXPos - MoveDist;
+      }else {
+        buggXPos = buggXPos + MoveDist;
+      }
+      buggNew.setAttribute("style","top:" + (buggXPos) + "px; left:"+ buggYPos+"px; border-bottom:4px solid white; background-color:"+bColor+'"');
+    }else if (rngMove === 4){
+      if (buggXPos <= 50){
+        buggXPos = buggXPos + MoveDist;
+      }else{
+        buggXPos = buggXPos - MoveDist;
+      }
+      buggNew.setAttribute("style","top:"+ (buggXPos) + "px; left:"+ buggYPos+"px; border-top:4px solid white; background-color:"+bColor+'"');
+    }
+
+  }, moveSpeed);
+}
+
+//Start Function
+function startFunction(count){
+  count = BuggPopulation;
+  startCount = startCount + 1;
+  if (startCount > 1 ||  startCount <1){
+    console.log("You've already started.");
+  }else{
+    let newBugg;
+     for (let i = 1; i<=count; i++){
+       newBugg = '<div id="bugg' + i +  '" class="bugg"><p class="buggMark">'+i+'</p></div>';
+       buggField.insertAdjacentHTML('beforeend', newBugg);
+       buggMovement("bugg".concat(i));
+    }
+  }
+}
---
+++ xoxoxo/xo_life.js
@@ -0,0 +1,190 @@
+// Xs and Os will use the basic js_life example to populate the screen with
+// two types of objects - X's and O's. Each object type is supposed to represent a
+// different sex, therefore the basic rules of thie simulation will be
+//
+// 1) When Xs and O's meet, they have a chance to populate the screen with another
+// object(ie birth offsrping). Initially, the sex of the offspring will be random.
+//
+// 2) When two X's cross paths, there will be a smaller chance that one is destrpyed
+// in the encounter. Initially, this too will be random but may change as traits
+// given to each type.
+//
+// Eventually properties such as random and inherited traits, social standing,
+// etc will be added in a very basic form.
+//
+
+//1) give each bugg a sex property
+//2) log encounter rates
+//3) move forward
+//
+
+
+const MoveDist = 10;
+const BaseSpeed = 600;
+const BuggPopulation = 10;
+
+let buggC = document.getElementsByClassName("bugg");
+let yPos = document.getElementById("yPos");
+let xPos = document.getElementById("xPos");
+let windowDim = document.getElementById("windowDim");
+let buggField = document.getElementById("buggField");
+let x = window.innerWidth;
+let y = window.innerHeight;
+let buggCount = 0;
+let startCount = 0;
+let buggyCount = 0;
+let newBugg;
+let buggArray = [];
+let collisonCount = 0;
+windowDim.innerHTML = "Width:  " + x +"  Height:  " + y;
+
+//Bugg Constuctor
+function Bugg (name){
+  buggyCount = buggyCount + 1;
+  let rngSpeedModifier;
+  let rngX;
+  let rngy;
+
+  rngX = rngControl(x);
+  rngY = rngControl(y);
+  buggR = rngControl(255);
+  buggG = rngControl(255);
+  buggB = rngControl(255);
+  rngSpeedModifier = rngControl(10);
+  buggSex = rngControl(2);
+  buggCount = buggCount + 1;
+
+  if (buggSex === 1){
+    this.sex = 'O';
+    this.buggColor = "rgb("+59+"," +70+","+ 241+");";
+  }else {
+    this.sex = 'X';
+    this.buggColor = "rgb("+230+"," +167+","+ 26+");";
+  }
+
+
+  this.name = name;
+  this.number = buggCount;
+  this.buggSpeed = BaseSpeed * (rngSpeedModifier/10);
+  this.health = rngControl(10);
+  this.startXY = [rngX, rngY];
+  // this.buggColor ="rgb("+buggR+","+ buggG+","+ buggB+");";
+
+
+  // this.stats = name +'s stats are: Speed: ['+
+  //             this.buggSpeed +'], ID:['+
+  //             this.number+'], XY POS: ['+
+  //             this.startXY + '],  health: ['+
+  //             this.health + '], color: ['+
+  //             this.buggColor +'], sex: ['+
+  //             this.sex +']';
+  // console.log(this.stats);
+}
+
+//RNG Controller
+function rngControl(number){
+  let newNum = Math.floor((Math.random() * number) + 1);
+  return newNum;
+}
+
+//Controls the random movement
+function buggMovement(buggID){
+  let buggMade = buggID;
+  buggEL = buggID;
+  buggMade = new Bugg(buggID);
+  let buggNew = document.getElementById(buggEL);
+  let moveSpeed = buggMade.buggSpeed;
+  let bColor = buggMade.buggColor;
+  let buggObject = {name: buggID,xPOS: 0,yPOS: 0};
+  buggArray.push(buggObject);
+  //set the random position inside start
+  let buggYPos = rngControl(x);
+  let buggXPos = rngControl(y);
+
+  setInterval(function(){
+    //will update the new buggObject properties at each interval
+    buggObject.xPOS = buggXPos;
+    buggObject.yPOS = buggYPos;
+
+    //keep this inside the setInterval function.
+    let rngMove = rngControl(4);
+    //stops everything if a bugs goes out of bounds
+    if (buggYPos >= x || buggYPos <= 0){
+      return 0;
+    }
+    if (buggXPos >= y || buggXPos <= 0){
+      return 0;
+    }
+
+    if (rngMove === 1){
+      if (buggYPos <= 50){
+        buggYPos = buggYPos + MoveDist;
+      }else{
+        buggYPos = buggYPos - MoveDist;
+      }
+      buggNew.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-left:4px solid white; background-color:"+bColor+'"');
+    }else if (rngMove === 2){
+      if (buggYPos >= x - 50){
+        buggYPos = buggYPos - MoveDist;
+      }else{
+        buggYPos = buggYPos + MoveDist;
+      }
+      buggNew.setAttribute("style","left:" + (buggYPos) + "px; top:"+ buggXPos+"px; border-right:4px solid white;  background-color:"+bColor+'"');
+    }else if (rngMove === 3){
+      if (buggXPos >= y - 50){
+        buggXPos = buggXPos - MoveDist;
+      }else {
+        buggXPos = buggXPos + MoveDist;
+      }
+      buggNew.setAttribute("style","top:" + (buggXPos) + "px; left:"+ buggYPos+"px; border-bottom:4px solid white; background-color:"+bColor+'"');
+    }else if (rngMove === 4){
+      if (buggXPos <= 50){
+        buggXPos = buggXPos + MoveDist;
+      }else{
+        buggXPos = buggXPos - MoveDist;
+      }
+      buggNew.setAttribute("style","top:"+ (buggXPos) + "px; left:"+ buggYPos+"px; border-top:4px solid white; background-color:"+bColor+'"');
+    }
+    return buggArray;
+  }, moveSpeed);
+}
+
+//collision detection function
+function buggPositionCheck(arrayData){
+  //needs much refinement
+  let checkSpeed = BaseSpeed;
+  let buggPop = BuggPopulation;
+
+  setInterval(function(){
+    for (i=0; i<= buggPop; i++){
+      for (j=i+1; j< buggPop; j++){
+         let bugg_data = ( arrayData[i].name + "[" + arrayData[i].xPOS+","+ arrayData[i].yPOS+"]," + arrayData[j].name + "[" + arrayData[j].xPOS +","+  arrayData[j].yPOS+"]" );
+         console.log(bugg_data);
+         // document.getElementById("bugg_data").innerHTML = bugg_data;
+        if( arrayData[i].xPOS <= arrayData[j].xPOS && arrayData[i].xPOS >= arrayData[j].xPOS-50 ){
+          if(arrayData[i].yPOS >= arrayData[j].yPOS - 50 && arrayData[i].yPOS <= arrayData[j].yPOS ){
+            collisonCount = collisonCount + 1;
+            document.getElementById("count").innerHTML = collisonCount;
+            console.log("POSIITON X MATCHES");
+          }
+        }
+      }
+    }
+  }, checkSpeed);
+}
+
+//Start Function
+function startFunction(count){
+  count = BuggPopulation;
+  startCount = startCount + 1;
+  if (startCount > 1 ||  startCount <1){
+    console.log("You've already started.");
+  }else{
+     for (let i = 1; i<=count; i++){
+       newBugg = '<div id="bugg' + i +  '" class="bugg"><p class="buggMark">'+i+'</p></div>';
+       buggField.insertAdjacentHTML('beforeend', newBugg);
+       buggMovement("bugg".concat(i));
+       buggPositionCheck(buggArray);
+    }
+  }
+}
